4d18c7789edd8fa2336ec63883eba2835b0aa5b6
Andrey Helldar <helldar@dragon-code.pro>
2026-01-10 23:24:16 +0300

    Simplify error handling in `index.ts` by refining `catch` block type annotations

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as core from '@actions/core'
 import previewUpdater from './main'
 
-previewUpdater().catch(error => {
-    const message = error instanceof Error ? error.message : String(error)
-
-    core.setFailed(`💥 Preview Updater failed with error: ${ message }`)
+previewUpdater().catch((error: Error) => {
+    core.setFailed(`💥 Preview Updater failed with error: ${ error.message }`)
 })
