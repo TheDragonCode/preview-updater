21bbbdeba75bdd133e2d8ede1d14638489b9c894
Andrey Helldar <helldar@dragon-code.pro>
2026-01-13 01:26:20 +0300

    Update `dev.ps1` to load `.env` variables, switch to `npm run build`, and integrate `act` for workflow testing

--- dev.ps1
+++ dev.ps1
@@ -2,4 +2,19 @@ $env:GITHUB_RUN_ID = 123
 $env:GITHUB_EVENT_NAME = "push"
 $env:GITHUB_EVENT_PATH = "TheDragonCode/preview-updater"
 
-npm run dev
+$envFile = ".env"
+
+Get-Content $envFile |
+        ForEach-Object {
+            if ($_ -match '^(?<key>[^=]+)=(?<val>.*)$')
+            {
+                $key = $Matches['key'].Trim()
+                $val = $Matches['val'].Trim()
+
+                $env: $key = $val
+            }
+        }
+
+npm run build
+
+act push -W '.github/workflows/preview.yml'
