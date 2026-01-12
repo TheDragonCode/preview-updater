b8f974941885700297fa1b766b3a7bc5a03607d5
Andrey Helldar <helldar@dragon-code.pro>
2026-01-12 17:59:49 +0300

    Remove `Image` type and integrate its properties into `Config` type for simplification

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,23 +1,30 @@
-export interface Config
+interface Image
 {
     url: string;
     parameters: object;
 }
 
+export interface Config
+{
+    image: Image;
+}
+
 export const defaultConfig: Config = {
-    url: 'https://banners.beyondco.de/{title}.png',
+    image: {
+        url: 'https://banners.beyondco.de/{title}.png',
 
-    parameters: {
-        theme: 'light',
-        pattern: 'topography',
-        style: 'style_2',
-        fontSize: '100px',
-        icon: 'https://laravel.com/img/logomark.min.svg',
+        parameters: {
+            theme: 'light',
+            pattern: 'topography',
+            style: 'style_2',
+            fontSize: '100px',
+            icon: 'https://laravel.com/img/logomark.min.svg',
 
-        packageManager: 'none',
-        packageName: '',
-        packageGlobal: false,
+            packageManager: 'none',
+            packageName: '',
+            packageGlobal: false,
 
-        description: ''
+            description: ''
+        }
     }
 }
