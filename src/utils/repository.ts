d924c4609273eafa4d7bcde72d215250f54150e6
Andrey Helldar <helldar@dragon-code.pro>
2026-01-14 11:57:47 +0300

    Fix commit message formatting to conditionally include body only if present

--- src/utils/repository.ts
+++ src/utils/repository.ts
@@ -84,10 +84,12 @@ export class Repository {
 
     async commit() {
         try {
-            const message =
-                this._config.repository.commit.title +
-                "\n" +
-                this._config.repository.commit.body;
+            let message = this._config.repository.commit.title;
+            const body = this._config.repository.commit.body || "";
+
+            if (body !== "") {
+                message += `\n${body}`;
+            }
 
             await exec(`git commit -m "${message}"`);
         } catch (error) {
